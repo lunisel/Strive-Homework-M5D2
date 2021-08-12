f53b39434eb7d8f706da6e47eb670cfcae0b7c60
Luna Sarmiento <selene942008@hotmail.com>
2021-08-12 10:05:50 +0200

    Re-do homeworks

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+import cors from "cors";
+
+import listEndpoints from "express-list-endpoints";
+
+import authorsRouter from "./authors/index.js";
+
+import blogsRouter from "./blogs/index.js";
+
+import { notFound, forbidden, catchAllErrorHandler } from "./errorHandlers.js";
+
+const server = express();
+
+const PORT = 3001;
+
+server.use(cors());
+
+server.use(express.json());
+
+server.use("/authors", authorsRouter);
+
+server.use("/blogs", blogsRouter);
+
+server.use(notFound);
+
+server.use(forbidden);
+
+server.use(catchAllErrorHandler);
+
+console.log(listEndpoints(server));
+
+server.listen(PORT, () => console.log("✅ Server is running on port : ", PORT));
+
+server.on("error", (error) =>
+  console.log(`❌ Server is not running due to : ${error}`)
+);
